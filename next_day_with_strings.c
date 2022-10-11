4f35332d6a1dbb72a70f22f14ca5bbf0cdd30ae2
Omar Mustafa <omarmustafa130@gmail.com>
2022-10-11 23:56:34 +0200

    problem 3 added

--- next_day_with_strings.c
+++ next_day_with_strings.c
@@ -1,84 +1,111 @@
 #include <stdio.h>
-typedef enum month
+#include <stdlib.h>
+
+typedef enum months      //declaring an enum of months
 {
     jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
 }month;
 
-typedef struct date
+typedef struct date     //struct date
 {
     month m;
     int day;
 }date;
 
-int arr[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+int MaxDaysInMonths[12] = {31,28,31,30,31,30,31,31,30,31,30,31};    //array that holds how many days each month have
+int MaxDays = 30;                                                   //Initial value of a variable that holds maximum days of a month
 
-char* GetEnumID(month m)
+#define NumberOfTestCases 5                                         //no of test cases
+
+
+date nextday(date d)                           //a function to get the next day
 {
-    switch (m)
+    date NextDay;                                   //the object of struct that will hold the next day
+    if (d.day < MaxDaysInMonths[d.m])                   //check if date entered is less than no of days of the month
     {
-        case jan: return "jan"; break;
-        case feb: return "feb"; break;
-        case mar: return "mar"; break;
-        case apr: return "apr"; break;
-        case may: return "may"; break;
-        case jun: return "jun"; break;
-        case jul: return "jul"; break;
-        case aug: return "aug"; break;
-        case sep: return "sep"; break;
-        case oct: return "oct"; break;
-        case nov: return "nov"; break;
-        case dec: return "dec"; break;
+        NextDay.m = d.m; NextDay.day = (d.day+1)%MaxDaysInMonths[d.m];          //next day = same month + (day + 1)
+        return NextDay;
     }
-}
-
-char* nextday(date d)
-{
-    int monthlen = 0, daylen=0, count =0;
-    char* FinalString;
-    char days [2];
-    if (d.day>32 || d.day<1)
+    if (d.day == MaxDaysInMonths[d.m])          //check if date entered is equal to no of days of the month
+    {
+        NextDay.m = (d.m + 1)%12; NextDay.day = 1;          //next day = (next month)%12 (so we never get past 12), day is the 1st day of the new month)
+        return NextDay;
+    }
+    if (d.day > MaxDaysInMonths[d.m])               //Error in input
     {
         printf("Error");
         return;
     }
-    if ((d.m == feb && d.day==28) || ((d.m == apr || d.m == jun || d.m == sep || d.m == nov) && d.day ==30))
-    {
-        d.m++;
-        d.day=0;
-    }
-    sprintf(days, "%d", d.day+1); //4,6,9,11
-    char* months=GetEnumID(d.m);
-    monthlen = strlen(months);
-    daylen = strlen(days);
-    FinalString = (char*) malloc(monthlen + daylen + 1);
-    strcpy(FinalString, months);
-    strcat(FinalString, ", ");
-    strcat(FinalString, days);
-    return FinalString;
 }
 
-void PrintDate(char* finalString)
+void PrintDate(date d)                           //a function that prints a date int this form: "March, 21" instead of "mar 21"
 {
-    int monthlen = 0, daylen=0, count =0;
-    char months[3] = {finalString[0], finalString[1], finalString[2]};
-    char days [2] = {finalString[5], finalString[6]};
-    if (strcmp(months, "jan")==0) printf("%s","January, ");
-    else if (strcmp(months, "feb")==0) printf("%s", "February, ");
-    else if (strcmp(months, "mar")==0) printf("%s", "March, ");
-    else if (strcmp(months, "apr")==0) printf("%s", "April, ");
-    else if (strcmp(months, "may")==0) printf("%s", "May, ");
-    else if (strcmp(months, "jun")==0) printf("%s", "June, ");
-    else if (strcmp(months, "jul")==0) printf("%s", "July, ");
-    else if (strcmp(months, "aug")==0) printf("%s", "August, ");
-    else if (strcmp(months, "sep")==0) printf("%s", "September, ");
-    else if (strcmp(months, "oct")==0) printf("%s", "October, ");
-    else if (strcmp(months, "nov")==0) printf("%s", "November, ");
-    else if (strcmp(months, "dec")==0) printf("%s", "December, ");
-    printf("%s", days);
+    switch(d.m)
+    {
+        case jan:
+            if(d.day>0 && d.day<=MaxDaysInMonths[0])        //check the input date for faulty inputs
+                printf("January, %d", d.day);
+            break;
+        case feb:
+            if(d.day>0 && d.day<=MaxDaysInMonths[1])
+                printf("February, %d", d.day);
+            break;
+        case mar:
+            if(d.day>0 && d.day<=MaxDaysInMonths[2])
+                printf("March, %d", d.day);
+            break;
+        case apr:
+            if(d.day>0 && d.day<=MaxDaysInMonths[3])
+                printf("April, %d", d.day);
+            break;
+        case may:
+            if(d.day>0 && d.day<=MaxDaysInMonths[4])
+                printf("May, %d", d.day);
+            break;
+        case jun:
+            if(d.day>0 && d.day<=MaxDaysInMonths[5])
+                printf("June, %d", d.day);
+            break;
+        case jul:
+            if(d.day>0 && d.day<=MaxDaysInMonths[6])
+                printf("July, %d", d.day);
+            break;
+        case aug:
+            if(d.day>0 && d.day<=MaxDaysInMonths[7])
+                printf("August, %d", d.day);
+            break;
+        case sep:
+            if(d.day>0 && d.day<=MaxDaysInMonths[8])
+                printf("September, %d", d.day);
+            break;
+        case oct:
+            if(d.day>0 && d.day<=MaxDaysInMonths[9])
+                printf("October, %d", d.day);
+            break;
+        case nov:
+            if(d.day>0 && d.day<=MaxDaysInMonths[10])
+                printf("November, %d", d.day);
+            break;
+        case dec:
+            if(d.day>0 && d.day<=MaxDaysInMonths[11])
+                printf("December, %d", d.day);
+            break;
+    }
 }
 int main()
 {
-    date d = {feb, 28};
-    PrintDate(nextday(d));
+    date d1 = {jan, 1};
+    date d2 = {feb, 28};
+    date d3 = {mar, 14};
+    date d4 = {oct, 31};
+    date d5 = {dec, 31};
+
+    date TestCases[NumberOfTestCases] = {d1,d2,d3,d4,d5};
+
+    for (int i = 0; i<NumberOfTestCases; i++)
+    {
+        PrintDate(nextday(TestCases[i]));
+        printf("\n");
+    }
     return 0;
 }
